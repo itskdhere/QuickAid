e9ef795ec91e5c3c9358c4e04d66bc950931bc38
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-02-28 13:35:01 +0530

    add react-icons & tailwind css

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,16 @@
-export default function Landing() {
+import { FaHourglassStart } from "react-icons/fa";
+
+function Landing() {
   return (
-    <>
-      <h1>Coming Soon...</h1>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="flex justify-center items-center gap-2 text-4xl">
+        <span>Coming Soon...</span>
+        <span>
+          <FaHourglassStart />
+        </span>
+      </h1>
+    </div>
   );
 }
+
+export default Landing;
