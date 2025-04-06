f7f0a2ac1efcfdabced0d29d872fd845e3cd9d77
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-04-06 22:31:38 +0530

    Update Dashboard.tsx

--- client/src/pages/user/Dashboard.tsx
+++ client/src/pages/user/Dashboard.tsx
@@ -135,8 +135,8 @@ export default function Dashboard() {
         <h2 className="text-xl font-semibold mb-4 flex items-center gap-2">
           <Heart className="w-5 h-5 text-pink-400" />
           <span>
-            Health Tips (Powered by
-            <Gemini className="inline h-6 ml-2 mr-1 -translate-y-1.5" />)
+            Health Tips Powered by
+            <Gemini className="inline h-6 ml-2 mr-1 -translate-y-1.5" />
           </span>
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
