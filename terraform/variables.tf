e4bc4a34c6d042da153583fd064e03993b2b47ed
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-06-18 02:45:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -42,8 +42,16 @@ variable "disk_image" {
 }
 
 variable "service_account_scopes" {
-  type    = list(string)
-  default = ["https://www.googleapis.com/auth/bigquery", "https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  type = list(string)
+  default = [
+    "https://www.googleapis.com/auth/bigquery",
+    "https://www.googleapis.com/auth/devstorage.read_only",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring.write",
+    "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/servicecontrol",
+    "https://www.googleapis.com/auth/trace.append",
+  ]
 }
 
 variable "service_account_email" {
